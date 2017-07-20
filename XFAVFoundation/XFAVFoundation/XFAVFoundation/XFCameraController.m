744b082bef5219dd2e989154ccbff785f0d922bb
LINGLemon <396955213@qq.com>
2017-07-20 19:03:50 +0800

    优化录制最大时间后录制按钮的动画

--- XFAVFoundation/XFAVFoundation/XFAVFoundation/XFCameraController.m
+++ XFAVFoundation/XFAVFoundation/XFAVFoundation/XFCameraController.m
@@ -746,6 +746,8 @@ typedef void(^PropertyChangeBlock)(AVCaptureDevice *captureDevice);
         return;
     }
     
+    [self.cameraButton setHidden:YES];
+    
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.3f * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         
         [weakSelf previewVideoAfterShoot];
