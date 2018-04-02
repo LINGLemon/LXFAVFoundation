f686ca7e8c4663f0635b03583b4ff8794dffd409
LINGLemon <396955213@qq.com>
2018-04-02 14:16:55 +0800

    新增异步线程实现视频录制

--- XFAVFoundation/XFAVFoundation/XFCameraController.m
+++ XFAVFoundation/XFAVFoundation/XFCameraController.m
@@ -809,14 +809,14 @@ typedef void(^PropertyChangeBlock)(AVCaptureDevice *captureDevice);
         __weak __typeof(self)weakSelf = self;
         if(_assetWriter && _assetWriter.status == AVAssetWriterStatusWriting)
         {
-            //        dispatch_async(self.videoQueue, ^{
-            [_assetWriter finishWritingWithCompletionHandler:^{
-                weakSelf.canWrite = NO;
-                weakSelf.assetWriter = nil;
-                weakSelf.assetWriterAudioInput = nil;
-                weakSelf.assetWriterVideoInput = nil;
-            }];
-            //        });
+            dispatch_async(self.videoQueue, ^{
+                [_assetWriter finishWritingWithCompletionHandler:^{
+                    weakSelf.canWrite = NO;
+                    weakSelf.assetWriter = nil;
+                    weakSelf.assetWriterAudioInput = nil;
+                    weakSelf.assetWriterVideoInput = nil;
+                }];
+            });
         }
         
         if (timeLength < VIDEO_RECORDER_MIN_TIME)
@@ -1349,8 +1349,8 @@ typedef void(^PropertyChangeBlock)(AVCaptureDevice *captureDevice);
         return;
     }
     
-//    CFRetain(sampleBuffer);
-//    dispatch_async(self.videoQueue, ^{
+    CFRetain(sampleBuffer);
+    dispatch_async(self.videoQueue, ^{
         @autoreleasepool
         {
             if (!self.canWrite && mediaType == AVMediaTypeVideo)
@@ -1391,10 +1391,9 @@ typedef void(^PropertyChangeBlock)(AVCaptureDevice *captureDevice);
                     }
                 }
             }
-            
-//            CFRelease(sampleBuffer);
+            CFRelease(sampleBuffer);
         }
-//    });
+    });
 }
 
 #pragma mark - 摄像头聚焦，与缩放
